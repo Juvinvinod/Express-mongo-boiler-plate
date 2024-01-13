5b23719e8055d90398c7359aa08391573b64f71b
juvin vinod <124287134+Juvinvinod@users.noreply.github.com>
2024-01-13 17:44:13 +0530

    connected db and express

---
+++ conifg/mongoDb.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const db = async() =>{
+    try {
+        await mongoose.connect(process.env.DATABASE);
+        console.log("Database connected!!");
+    } catch (error) {
+        console.log("Database connection error!!!")
+    }
+}
+
+module.exports = db;
